b313569389f1ce11a84aa7baaeeac65ba17551d3
Ernesto Flores <eflores73@students.avc.edu>
2024-02-21 23:20:52 -0800

    Done

--- main.hpp
+++ main.hpp
@@ -13,13 +13,21 @@ int getClosestMed(int, int, int);
 // complete two functions
 int getRandom(void)
 {
-        /***************************************************
-         * Code your program here
-         ***************************************************/
+        return rand() % 100;
 }
 int getClosestMed(int rdnum1, int rdnum2, int rdnum3)
 {
-        /***************************************************
-         * Code your program here
-         ***************************************************/
+        double mean = (rdnum1 + rdnum2 + rdnum3) / 3.0;
+
+        double diff1 = abs(mean - rdnum1);
+        double diff2 = abs(mean - rdnum2);
+        double diff3 = abs(mean - rdnum3);
+
+        if (diff1 <= diff2 && diff1 <= diff3) {
+                return rdnum1;
+        } else if(diff2 <= diff1 && diff2 <= diff3) {
+                return rdnum2;
+        } else {
+                return rdnum3;
+        }
 }
